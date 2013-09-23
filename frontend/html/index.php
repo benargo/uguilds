3ef2e588b04349ec0e246618f5df7345b5718d34
Ben Argo <ben@benargo.com>
2013-09-23 23:00:08 +0100

    Minor frontend preparations

---
+++ frontend/html/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="robots" content="all">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<title>Welcome to Uguilds - Intelligent Guild Websites</title>
+
+	<link rel="stylesheet" media="screen" href="css/basic.css">
+	<link type="image/x-icon" rel="shortcut icon" href="../images/favicon.ico">
+	<link rel="apple-touch-icon" href="/media/images/iphone-icon.png">
+</head>
+<body>
+	<header>
+		<hgroup>
+			<a href="./" rel="home">
+				<h1>Uguilds</h1>
+				<h2>Intelligent Guild Hosting</h2>
+			</a>
+		</hgroup>
+	</header>
+</body>
+</html>
