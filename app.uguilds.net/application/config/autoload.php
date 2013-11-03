c1363de88d5ff760eaa08ec93635952028c40cff
Ben Argo <ben@benargo.com>
2013-11-03 08:45:51 +0000

    Updating config

--- app.uguilds.net/application/config/autoload.php
+++ app.uguilds.net/application/config/autoload.php
@@ -53,7 +53,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('Couchdb','BattlenetArmory','uGuilds');
+$autoload['libraries'] = array('database','session','BattlenetArmory','uGuilds');
 
 /*
 | -------------------------------------------------------------------
