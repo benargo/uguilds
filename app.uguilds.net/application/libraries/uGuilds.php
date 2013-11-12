e2278880f3ae613ef67c691272a82fd42e054725
Ben Argo <ben@benargo.com>
2013-11-12 21:30:23 +0000

    Maybe it's the class name?

--- app.uguilds.net/application/libraries/uGuilds.php
+++ app.uguilds.net/application/libraries/uGuilds.php
@@ -14,7 +14,7 @@ require_once APPPATH . 'libraries/uGuilds/Classes.php';
 require_once APPPATH . 'libraries/uGuilds/Theme.php';
 require_once APPPATH . 'libraries/uGuilds/ThemeData.php';
 
-class Uguilds {
+class uGuilds {
 
 	/**
 	 * vars
